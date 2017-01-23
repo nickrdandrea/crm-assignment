8cb17113aa9eaf8e7f54dad90efacf1194a79af0
nickrdandrea <nickrdandrea@gmail.com>
2017-01-23 10:33:13 -0500

    Add initial code

--- crm.rb
+++ crm.rb
@@ -1,15 +1,27 @@
+require_relative 'contact'
+
 class CRM
 
   def initialize
-
+    main_menu
   end
 
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
   def call_option
@@ -29,12 +41,14 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
 end
+
+tests = CRM.new()
