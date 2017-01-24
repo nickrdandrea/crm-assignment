ff44c61ce8b21a915ea4c89b074b9c7c1cb1137c
nickrdandrea <nickrdandrea@gmail.com>
2017-01-23 23:08:10 -0500

    Nearly completed Contact

--- contact.rb
+++ contact.rb
@@ -6,18 +6,20 @@ class Contact
   @@id = 1
 
   # This method should initialize the contact's attributes
-  def initialize
-    def initialize(first_name, last_name, email, note)
+  def initialize(first_name, last_name, email, note)
     @first_name = first_name
     @last_name = last_name
     @email = email
     @note = note
-    end
+
+    @id = @@id
+    @@id += 1
+
   end
 
   # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
+  def self.create(first_name, last_name, email, note)
     new_contact = Contact.new(first_name, last_name, email, note)
     @@contacts << new_contact
     return new_contact
@@ -25,19 +27,55 @@ class Contact
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts.each do |i|
+      puts i.first_name + " " + i.last_name
+    end
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    i = @@contacts.find_index {|s| s.id == id}
+    puts @@contacts[i].first_name + " " + @@contacts[i].last_name + " [#{id}]"
   end
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
+
+  # This method allows update with a contact id
+  # probably moving this to crm
+  def self.update(id)
+    i = @@contacts.find_index {|s| s.id == id}
+    puts '[1] First Name'
+    puts '[2] Last Name'
+    puts '[3] Email'
+    puts '[4] Note'
+
+    puts case gets.chomp.to_i
+    when 1
+      puts "Current first name: " + @@contacts[i].first_name
+      puts "Enter new first name:"
+      @@contacts[i].first_name = gets.chomp
+    when 2
+      puts "Current last name:" + @@contacts[i].last_name
+      puts "Enter new name:"
+      @@contacts[i].last_name = gets.chomp
+    when 3
+      puts "Current email: " + @@contacts[i].email
+      puts "Enter new email:"
+      @@contacts[i].email = gets.chomp
+    when 4
+      puts "Current note: " + @@contacts[i].note
+      puts "Enter new note:"
+      @@contacts[i].note = gets.chomp
+    else
+      "Invalid option selected."
+      Contact.update(id)
+    end
+  end
+
   def update
 
   end
@@ -46,25 +84,40 @@ class Contact
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
+  def self.find_by(attribute, value)
+    #WIP
+    i = @@contacts.find_index {|s| s.attribute == value}
+    puts @@contacts[i].first_name + " " + @@contacts[i].last_name + " [#{id}]"
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts.clear
   end
 
   def full_name
-
+    puts @first_name + " " + @last_name
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
+    @@contacts.delete(self)
+  end
 
+  def self.foo(i)
+    puts @@contacts[i].first_name
   end
 
   # Feel free to add other methods here, if you need them.
 
 end
+
+
+betty = Contact.create('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+timmy = Contact.create('Timmy', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+amy = Contact.create('Amy', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+
+Contact.all
+#Contact.update(2)
+Contact.find_by(send(:first_name, "Betty"))
